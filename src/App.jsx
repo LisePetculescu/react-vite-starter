ba364b48dc551e00238ac0b1e562cd953dd5ef15
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-09-28 11:46:18 +0200

    hihi

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import reactLogo from "./assets/react.svg";
 
 function App() {
-    return (
-        <>
-            <header>
-                <a href="https://react.dev" target="_blank" rel="noreferrer">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-                <h1>RACE Your React 🎉</h1>
-            </header>
-        </>
-    );
+  return (
+    <>
+      <header>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <h1>RACE Your React 🎉</h1>
+      </header>
+      <Member></Member>
+    </>
+  );
+}
+
+function Member() {
+  return <h2>Hej member</h2>;
 }
 
 export default App;
